fb2b72a04f5392dacf5b452c24b180bb1b67c795
Airinda <airinda05@gmail.com>
2025-09-10 14:32:02 +0800

    Day3komentar.java

--- Day3komentar.java
+++ Day3komentar.java
@@ -16,14 +16,15 @@ public class Day3Ngoding {
 
         float a = 265.25f;
         System.out.println("sebelumupdate nilainya salah" + a);
-        /*nilai rata rata yang di berikan salah jadi kita harus memperbarui
+        //nilai rata rata yang di berikan salah jadi kita harus memperbarui
         a = 82.25f ;
         System.out.println("sesudah di update nilai rata rata yang di perbarui sudah benar");
         //nilai rata rata yang telah di perbarui sudah benar
         final float b = 82.25f ;
         System.out.println("jawaban akhir yang fiks"+b);
-        */final jawaban dan sudah tidak bisa diperbarui lagi karena hasil setelahnya akan error
-       
+        /*
+        final jawaban dan sudah tidak bisa diperbarui lagi karena hasil setelahnya akan error
+       */
 
     }
 }
