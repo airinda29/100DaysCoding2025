6e2f4c55037ebc84b7bbb9e699f6dd487315ebe0
Airinda <airinda05@gmail.com>
2025-09-10 13:19:25 +0800

    Day3komentar.java

---
+++ Day3komentar.java
@@ -0,0 +1,29 @@
+public class Day3Ngoding {
+
+    public static void main(String[] args) {
+        System.out.println("menemukan rata rata nilai matematika saya selama 4 semester;");
+        byte sem1 = 79;
+        byte sem2 = 83;
+        byte sem3 = 82;
+        byte sem4 = 85;
+        byte jumlah = 4;
+
+        System.out.printf("semester 1: %d\nsemester 2: %d\nsemester 3: %d\nsemester 4; %d", sem1, sem2, sem3, sem4);
+
+        int ratarata = sem1 + sem2 + sem3 + sem4 / 4;
+        System.out.printf("\nrata rata nilai matematika saya:%d%n", ratarata);
+        //berikan rata rata nilai matematika saya selama 4 semester
+
+        float a = 265.25f;
+        System.out.println("sebelumupdate nilainya salah" + a);
+        //nilai rata rata yang di berikan salah jadi kita harus memperbarui
+        a = 82.25f ;
+        System.out.println("sesudah di update nilai rata rata yang di perbarui sudah benar");
+        //nilai rata rata yang telah di perbarui sudah benar
+        final float b = 82.25f ;
+        System.out.println("jawaban akhir yang fiks"+b);
+        //final jawaban dan sudah tidak bisa diperbarui lagi karena hasil setelahnya akan error
+       
+
+    }
+}
